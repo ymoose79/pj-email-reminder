d87bfdf4f0b898527599fd23cc004526d0eda750
ymoose79 <justin.r.stock@icloud.com>
2022-03-31 16:30:15 -0400

    good to go

--- src/UI/Button.svelte
+++ src/UI/Button.svelte
@@ -1,23 +1,22 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    const dispatch = createEventDispatcher();
-
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 </script>
 
 <div class="container">
-    <button>Update the UI</button>
+	<button>Update the UI</button>
 </div>
 
 <style>
-    .container{
-        position: relative;
-    }
-    button{
-       display: block;
-       font-size: 2em;
-        margin: 2rem auto;
-        padding: 1rem;
-        width: 25rem;
-        background-color: rgb(52, 202, 89);
-    }
+	.container {
+		position: relative;
+	}
+	button {
+		display: block;
+		font-size: 2em;
+		margin: 2rem auto;
+		padding: 1rem;
+		width: 25rem;
+		background-color: rgb(52, 202, 89);
+	}
 </style>
