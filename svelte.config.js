38269a1c0826655b5e092d29908a33f393465c3c
ymoose79 <justin.r.stock@icloud.com>
2022-04-01 09:04:04 -0400

    good to go?...

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from "svelte-preprocess";
-// import node from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-netlify';
+import node from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-netlify';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-        adapter: adapter(),
+        adapter: node(),
         // ********* ------> added as part of utube linking mondgoDB
         
 	},
