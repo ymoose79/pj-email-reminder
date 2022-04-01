ac7115152659532c43e4146432d08b36df2661b2
ymoose79 <justin.r.stock@icloud.com>
2022-04-01 09:28:35 -0400

    brought back the old config.  deleted netlify adap

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
 import preprocess from "svelte-preprocess";
 import node from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-netlify';
+// import vercel from '@sveltejs/adapter-vercel'
+
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
-        adapter: node(),
-        // ********* ------> added as part of utube linking mondgoDB
+    // kit: {
+	// 	// hydrate the <div id="svelte"> element in src/app.html
+    //     target: '#svelte',
+	// 	adapter: vercel()
+    //     // ********* ------> added as part of utube linking mondgoDB
         
-	},
+	// },
+     kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: node()
+    } ,
 
     preprocess: [preprocess({
         postcss: true
